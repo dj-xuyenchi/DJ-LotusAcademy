9467d0e79ccaf191e2b859523e904c52086040ff
Bùi Hiếu <buichihieu1321999@gmail.com>
2023-02-22 13:15:58 +0700

    fix bug

--- dj-client/src/components/Business/StudentManager/Evalute/StudentDetail/StudentDetail.tsx
+++ dj-client/src/components/Business/StudentManager/Evalute/StudentDetail/StudentDetail.tsx
@@ -1,67 +1,57 @@
-import Button from "react-bootstrap/Button";
-import Table from "react-bootstrap/Table";
-import './StudentDetail.css'
-import {
-    FaPinterestP,
-    FaFacebookF,
-    FaTwitter,
-    FaLinkedinIn,
-} from "react-icons/fa";
-import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import './StudentDetail.css';
+import zalo from '../../../../../assets/icons/zalo-icon.svg';
+import facebook from '../../../../../assets/icons/facebook-icon.svg';
+import { useEffect, useState } from 'react';
 function StudentDetail() {
     const [studentDetail, setStudentDetail] = useState({
-        avatar: "",
-        name: "Thái Lan Hương",
-        email: "huongkute@gmail.com",
-        phone: "+84 968491797",
-        location: "Thái Thụy - Thái Bình",
-        birthday: "09-02-1999",
-        gender: "Nữ",
-        zalo: "http://zalo.com/123",
-        facebook: "http://facebook.com/anhlin",
-        scoreBoard: [{
-            lesson: "Java Method",
-            score: 10,
-            dateTime: "02-1-2023",
-            employeeCheck: "Sếp Bình",
-            evaluate: "Đạt",
-            linkStudentTest: "gggdrive/123"
-        }, {
-            lesson: "C# Method",
-            score: 10,
-            dateTime: "02-1-2023",
-            employeeCheck: "Sếp Bình",
-            evaluate: "Đạt",
-            linkStudentTest: "gggdrive/123"
-        },]
-    })
+        avatar: '',
+        name: 'Thái Lan Hương',
+        email: 'huongkute@gmail.com',
+        phone: '+84 968491797',
+        location: 'Thái Thụy - Thái Bình',
+        birthday: '09-02-1999',
+        gender: 'Nữ',
+        zalo: 'http://zalo.com/123',
+        facebook: 'http://facebook.com/anhlin',
+        scoreBoard: [
+            {
+                lesson: 'Java Method',
+                score: 10,
+                dateTime: '02-1-2023',
+                employeeCheck: 'Sếp Bình',
+                evaluate: 'Đạt',
+                linkStudentTest: 'gggdrive/123',
+            },
+            {
+                lesson: 'C# Method',
+                score: 10,
+                dateTime: '02-1-2023',
+                employeeCheck: 'Sếp Bình',
+                evaluate: 'Đạt',
+                linkStudentTest: 'gggdrive/123',
+            },
+        ],
+    });
     return (
-        <div className="StudentDetail w-100" style={{ padding: "24px 32px" }}>
+        <div className="StudentDetail w-100" style={{ padding: '24px 32px' }}>
             <div className="detail-title">
                 <h3>Student Detail</h3>
             </div>
-            <div style={{ display: "flex" }}>
-                <div
-                    className="card"
-                    style={{ width: "30%", padding: "20px" }}
-                >
+            <div style={{ display: 'flex' }}>
+                <div className="card" style={{ width: '30%', padding: '20px' }}>
                     <div className="avatar" style={{ display: 'flex', justifyContent: 'center' }}>
                         <img
                             src="https://elstar.themenate.net/img/avatars/thumb-1.jpg"
                             alt=""
-                            style={{ borderRadius: "50%", height: "100px", width: "100px", alignItems: 'center' }}
+                            style={{ borderRadius: '50%', height: '100px', width: '100px', alignItems: 'center' }}
                         />
                     </div>
-                    <div className="name" style={{ alignItems: "center", margin: "0 auto" }}>
-                        <h4 style={{ marginTop: "20px", fontWeight: "bold" }}>
-                            {studentDetail.name}
-
-                        </h4>
+                    <div className="name" style={{ alignItems: 'center', margin: '0 auto' }}>
+                        <h4 style={{ marginTop: '20px', fontWeight: 'bold' }}>{studentDetail.name}</h4>
                     </div>
-                    <div
-                        className="info"
-                        style={{ marginTop: "32px", fontSize: "14px" }}
-                    >
+                    <div className="info" style={{ marginTop: '32px', fontSize: '14px' }}>
                         <div>
                             <span>Email</span>
                             <p>{studentDetail.email}</p>
@@ -84,102 +74,48 @@ function StudentDetail() {
                         </div>
                         <div className="icons-social">
                             <span>Social</span>
-                            <div style={{ display: "flex" }}>
-                                <div className="icon-cirle"
-                                    style={{
-                                        width: "30px",
-                                        height: "30px",
-                                        borderRadius: "50%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        border: "1px solid #ccc",
-                                    }}
-                                >
-                                    <FaPinterestP size={"20px"} />
+                            <div style={{ display: 'flex' }}>
+                                <div className="icon-cirle">
+                                    <img src={zalo} alt="" />
                                 </div>
-                                <div className="icon-cirle"
-                                    style={{
-                                        width: "30px",
-                                        height: "30px",
-                                        borderRadius: "50%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        border: "1px solid #ccc",
-                                    }}
-                                >
-                                    <FaFacebookF size={"20px"} />
-                                </div>
-                                <div className="icon-cirle"
-                                    style={{
-                                        width: "30px",
-                                        height: "30px",
-                                        borderRadius: "50%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        border: "1px solid #ccc",
-                                    }}
-                                >
-                                    <FaTwitter size={"20px"} />
-                                </div>
-                                <div className="icon-cirle"
-                                    style={{
-                                        width: "30px",
-                                        height: "30px",
-                                        borderRadius: "50%",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        border: "1px solid #ccc",
-                                    }}
-                                >
-                                    <FaLinkedinIn size={"20px"} />
+                                <div className="icon-cirle">
+                                    <img src={facebook} alt="" />
                                 </div>
                             </div>
                         </div>
-                        <div style={{ display: "flex", marginTop: "20px" }}>
+                        <div style={{ display: 'flex', marginTop: '20px' }}>
                             <Button
                                 variant="outline-secondary"
                                 style={{
-                                    width: "50%",
-                                    height: "36px",
-                                    marginRight: "10px",
+                                    width: '50%',
+                                    height: '36px',
+                                    marginRight: '10px',
                                 }}
                             >
                                 Delete
                             </Button>
-                            <Button variant="primary" style={{ width: "50%" }}>
+                            <Button variant="primary" style={{ width: '50%' }}>
                                 Edit
                             </Button>
                         </div>
                     </div>
                 </div>
-                <div style={{ marginLeft: "20px", width: "100%" }}>
-                    <div style={{ marginBottom: "32px" }}>
-                        <h6 style={{ fontSize: "16px", marginBottom: "16px" }}>
-                            Sale
-                        </h6>
+                <div style={{ marginLeft: '20px', width: '100%' }}>
+                    <div style={{ marginBottom: '32px' }}>
+                        <h6 style={{ fontSize: '16px', marginBottom: '16px' }}>Sale</h6>
                         <div
                             style={{
-                                height: "85px",
-                                backgroundColor: "#ffff",
-                                borderRadius: "20px",
-                                border: "1px solid rgba(0, 0, 0, 0.175)"
+                                height: '85px',
+                                backgroundColor: '#ffff',
+                                borderRadius: '20px',
+                                border: '1px solid rgba(0, 0, 0, 0.175)',
                             }}
-                        >
-
-
-
-                        </div>
+                        ></div>
                     </div>
-                    <h6 style={{ fontSize: "16px", marginBottom: "16px" }}>
-                        Bảng điểm
-                    </h6>
+                    <h6 style={{ fontSize: '16px', marginBottom: '16px' }}>Bảng điểm</h6>
                     <Table className="table-default" hover>
                         <thead>
-                            <tr style={{ backgroundColor: "#ccc" }}>
+                            <tr style={{ backgroundColor: '#ccc' }}>
                                 <th>
                                     <span>Học phần</span>
                                 </th>
@@ -200,7 +136,7 @@ function StudentDetail() {
                                 </th>
                             </tr>
                         </thead>
-                        <tbody>
+                        {/* <tbody>
                             {studentDetail.scoreBoard.map((item) => {
                                 return `<tr>
                                 <th>
@@ -221,9 +157,9 @@ function StudentDetail() {
                                 <th>
                                     <span>${item.linkStudentTest}</span>
                                 </th>
-                            </tr>`
+                            </tr>`;
                             })}
-                        </tbody>
+                        </tbody> */}
                     </Table>
                 </div>
             </div>
