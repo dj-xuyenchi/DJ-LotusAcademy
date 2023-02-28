a6abf38cf993f90b3522661822ea5d2b34f2e5c5
dj-xuyenchi <do.quanganh99zz@gmail.com>
2023-03-01 00:50:31 +0700

    update meoai

--- dj-client/src/Layout/MeoAiRoute.ts
+++ dj-client/src/Layout/MeoAiRoute.ts
@@ -1,23 +1,14 @@
 export const MeoAI =
 {
-    "bat ngo": "./MeoAIReact/batngo.gif",
-
-    "buc minh": "./MeoAIReact/bucminh.gif",
-
-    "khoc": "./MeoAIReact/huhu.gif",
-
-    "khong quan tam": "./MeoAIReact/khongquantam.gif",
-
-    "mai yeu": "./MeoAIReact/maiyeu.gif",
-
-    "ngauchua": "./MeoAIReact/ngauchua.gif",
-
-    "ok dong y": "./MeoAIReact/okdongy.gif",
-    "so hai": "./MeoAIReact/khongquantam.gif"
-
-    , "troi oi xinh qua": "./MeoAIReact/troioixinhqua.gif"
-
-    , "xin chao": "./MeoAIReact/xin chao.gif"
-
-    , "quay len": "./MeoAIReact/quaylen.gif"
+    batngo: "./MeoAIReact/batngo.gif",
+    bucminh: "./MeoAIReact/bucminh.gif",
+    khoc: "./MeoAIReact/huhu.gif",
+    khongquantam: "./MeoAIReact/khongquantam.gif",
+    maiyeu: "./MeoAIReact/maiyeu.gif",
+    ngauchua: "./MeoAIReact/ngauchua.gif",
+    okdongy: "./MeoAIReact/okdongy.gif",
+    sohai: "./MeoAIReact/khongquantam.gif",
+    troioixinhqua: "./MeoAIReact/troioixinhqua.gif",
+    xinchao: "./MeoAIReact/xinchao.gif",
+    quaylen: "./MeoAIReact/quaylen.gif"
 }
